ca47ba95119d311a05e335975aa35032c01b0fe1
Govindu Harshana <harshanagovindu@gmail.com>
2025-05-08 23:39:44 +0530

    Create Particle Effects Pot animations and Pot disappear script
    Create Pot Up down rotate Animation
    
    Create BreakablePot.cs for stick touch the pot disappear the pot and start magic effect.
    
    Import Magic effect package.

--- Assets/MyScripts/BreakablePot.cs
+++ Assets/MyScripts/BreakablePot.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BreakablePot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ParticleSystem breakParticles;
+    public AudioClip breakSound;
+    private Rigidbody rb;
+    private Animator anim;
+    private XRGrabInteractable grabInteractable;
+    private bool isBroken = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BreakPot()
     {
-        
+        if (isBroken) return;
+
+        isBroken = true;
+
+        // Disable interaction
+        if (grabInteractable != null)
+            grabInteractable.enabled = false;
+
+        // Play effects
+        if (breakParticles != null)
+        {
+            ParticleSystem particles = Instantiate(breakParticles, transform.position, Quaternion.identity);
+            particles.Play();
+            Destroy(particles.gameObject, particles.main.duration);
+        }
+
+        if (breakSound != null)
+            AudioSource.PlayClipAtPoint(breakSound, transform.position);
+
+        // Disable the pot
+        gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Break immediately on any stick contact
+        if (collision.gameObject.CompareTag("Stick"))
+        {
+            BreakPot();
+        }
     }
 }
